01f72793b9e800bb6e3eb80858e2ca1c0a4deca3
GAVYA SREE U <49972026+Gavyasree12@users.noreply.github.com>
2023-09-11 21:03:07 +0530

    changes done

--- healthcare/src/Changeserv.java
+++ healthcare/src/Changeserv.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Changeserv
- */
+
 @WebServlet("/Changeserv")
 public class Changeserv extends HttpServlet {
 	private static final long serialVersionUID = 1L;
